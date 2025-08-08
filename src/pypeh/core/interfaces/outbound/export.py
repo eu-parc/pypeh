4e6396dcf3f626f72f5c8210b218807cef7b5ee6
Dirk Devriendt <dirk.devriendt.ext@vito.be>
2025-08-08 15:02:53 +0200

    Clean up export interface

--- src/pypeh/core/interfaces/outbound/export.py
+++ src/pypeh/core/interfaces/outbound/export.py
@@ -7,24 +7,19 @@ from __future__ import annotations
 import logging
 
 from abc import abstractmethod
-from peh_model.peh import DataLayout, EntityList
-from typing import TYPE_CHECKING, TypeVar, Generic, cast, List
-
-if TYPE_CHECKING:
-    from typing import Sequence
-    from pypeh.core.models.validation_errors import ValidationErrorReport
 
 logger = logging.getLogger(__name__)
 
 class ExportInterface:
     @abstractmethod
-    def export_data(self) -> bool:
+    def export_data_template(self, layout, destination: str, observable_property_dict: dict = None, studyinfo_header_list: list = None, codebook_metadata_dict: dict = None) -> bool:
         raise NotImplementedError
 
     @abstractmethod
-    def export_data_dictionary(self) -> bool:
+    def export_data_dictionary(self, observation_design, layout, destination: str, observable_property_dict: dict = None, studyinfo_header_list: list = None, codebook_metadata_dict: dict = None) -> bool:
         raise NotImplementedError
 
     @abstractmethod
-    def export_data_template(self) -> bool:
+    def export_data(self, observation_result, layout, destination: str, observable_property_dict: dict = None, studyinfo_header_list: list = None, codebook_metadata_dict: dict = None) -> bool:
         raise NotImplementedError
+
